7855ca30fdc148fba15fd5991f01658cda7adf51
早新 <ohiyox@gmail.com>
2020-02-15 17:48:00 +0800

    none

--- die_visual.py
+++ die_visual.py
@@ -1,5 +1,6 @@
 from die import Die
 import matplotlib.pyplot as plt
+import pygal
 
 # 创建一个D6
 die = Die()
@@ -17,8 +18,19 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
+# 对结果进行可视化
+hist = pygal.Bar()
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist._y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_vusual.svg')
+
+
 x_values = list(range(1, 7))
-plt.ylim(ymin=15000,ymax=max(frequencies)*1.1)
+plt.ylim(ymin=15000, ymax=max(frequencies)*1.1)
 
 plt.plot(x_values, frequencies, linewidth=2)
 plt.show()
